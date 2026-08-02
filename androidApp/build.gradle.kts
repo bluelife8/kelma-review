1562e9a9b70e7ee62e3db6301169a5e8c0565d8e
Kelma Tech LLC <311893763+bluelife8@users.noreply.github.com>
2026-08-01 20:07:06 -0400

    Prepare Kelma Review 1.0.14

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "tech.kelma.app"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 13
-        versionName = "1.0.13"
+        versionCode = 14
+        versionName = "1.0.14"
         externalNativeBuild {
             cmake {
                 arguments += "-DANDROID_STL=none"
